79d5a17c7f969afcd6a286fbd155d3f74279525e
Nicolas M. Thiéry <nthiery@users.sf.net>
2019-10-30 08:40:17 +0100

    Updated

--- style/odk.py
+++ style/odk.py
@@ -3,5 +3,14 @@ from IPython.display import display, HTML
 display(HTML("""
 <style>
   I.logo::before{content:url(./pictures/logo-odk.png)}
+  .modular {
+      color: #ff0000;
+  }
+  .semigroup {
+      color: #0000ff;
+  }
+  .sym {
+      color: #009900;
+  }
 </style>
 """))
